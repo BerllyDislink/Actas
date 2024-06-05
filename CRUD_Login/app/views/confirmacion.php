a412daf159baf805f5972d25a89bbaf140d45727
BerllyDislink <josefrank1905@hotmail.com>
2024-06-05 14:15:24 -0500

    Actas v1.1

--- CRUD_Login/app/views/confirmacion.php
+++ CRUD_Login/app/views/confirmacion.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-include_once 'header.php'
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmar correo</title>
-    <link rel="stylesheet" href="../../public/css/login.css">
+    <link rel="stylesheet" href="../../public/css/confirmacion.css">
 </head>
 
 <?php
 include_once "../../app/helpers/sesion_helper.php";
 ?>
 
-<body class="inicio">
+<body>
     <div class="container">
-        <label for="Codigo">Ingrese el codigo de verificación que fue enviado a su correo</label>
+        <div class="content-area">
+        <h1>Se ha enviado un código de confirmación a<br> tu correo electrónico</h1>
         <form method="post" action="../controllers/ControllerSesion.php">
             <?php flash('confirmar'); ?>
             <input type="hidden" name="type" value="confirmar">
             <input type="text" class="form-control form-input" id="codigo" name="codigo" placeholder="Ingresar codigo de verificación">
             <button type="submit">Ingresar</buform>
         </form>
+    </div>
     </div>
     <script></script>
 </body>
